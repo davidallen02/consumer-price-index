7d59995003d069dc1179d09fd37c2382dfce1c09
David Allen <davidallen02@me.com>
2020-10-19 07:14:16 -0700

    add annual percent change by category

---
+++ R/category-annual-pchange.R
@@ -0,0 +1,42 @@
+library(magrittr)
+
+dat <- pamngr::join_sheets(c("cpsfhome",
+                             "cpiqfafs",
+                             "cpupencm",
+                             "cpshge",
+                             "cpstnv",
+                             "cpstuctr",
+                             "cpsctot",
+                             "cpumcmdy",
+                             "cpshshlt",
+                             "cpsstran",
+                             "cpumserv")) %>%
+  reshape2::melt(id.vars = "dates") %>%
+  pamngr::pchange(k = 12) %>%
+  dplyr::slice_max(dates, n = 1)
+
+period <- dat %>% dplyr::select(dates) %>% dplyr::pull() %>% unique() %>% format ("%B %Y")
+
+key <- readRDS("data/key.RDS") 
+
+cat_names <- key %>%
+  dplyr::mutate(
+    full = LONG_COMP_NAME %>% 
+      stringr::str_remove_all("US CPI Urban Consumers ") %>%
+      stringr::str_remove_all("US CPI ") %>%
+      stringr::str_remove(" SA") %>%
+      stringr::str_replace_all(" ", "\n"),
+    variable = security
+  ) %>%
+  dplyr::select(variable, full)
+
+p <- dat %>%
+  dplyr::mutate(dates = dates %>% as.character()) %>%
+  dplyr::left_join(cat_names, by = "variable") %>%
+  pamngr::barplot(x = "full", y = "value", fill = "dates") %>%
+  pamngr::pam_plot(
+    plot_title = "Consumer Price Index",
+    plot_subtitle = paste(period, "Annual Percent Change, Seasonally Adjusted"),
+    show_legend = FALSE
+  ) %>%
+  pamngr::all_output("category_annual_pchange")

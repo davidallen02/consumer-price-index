fccf05f62cab1e1e5999dd5f0a5d65973f4c19de
David Allen <davidallen02@me.com>
2020-10-19 06:55:02 -0700

    break up code

--- R/headline-monthly-pchange.R
+++ R/headline-monthly-pchange.R
@@ -1,11 +1,14 @@
-pamngr::get_data("cpi indx") %>%
+dat <- pamngr::get_data("cpi indx") %>%
   reshape2::melt(id.vars = "dates") %>%
   pamngr::pchange(k = 1) %>%
-  dplyr::slice_max(dates, n = 36) %>% 
+  dplyr::slice_max(dates, n = 36)
+
+p <- dat %>%
   pamngr::barplot(fill = "variable") %>%
   pamngr::pam_plot(
     plot_title = "Consumer Price Index",
     plot_subtitle = "Monthly Percent Change, Seasonally Adjusted",
     show_legend = FALSE
-  ) %>%
-  pamngr::all_output("headline-monthly-pchange")
+  )
+
+p %>% pamngr::all_output("headline-monthly-pchange")
